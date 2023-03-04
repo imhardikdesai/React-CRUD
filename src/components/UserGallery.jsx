9257be9ed0bcd5eec8796da690134852a4b56c63
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2023-03-04 17:31:54 +0530

    add loader

--- src/components/UserGallery.jsx
+++ src/components/UserGallery.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import UserBox from "./UserBox";
-import demoData from "./../data/demo-data";
+import { ThreeDots } from "react-loader-spinner";
 import { fetchGame } from "../Redux/userActions";
 import { useDispatch, useSelector } from "react-redux";
 
 const UserGallery = () => {
   const dispatch = useDispatch()
   const userData = useSelector(state => state.userData)
+  const loading = useSelector(state => state.loading)
   useEffect(() => {
     dispatch(fetchGame())
   }, [dispatch])
@@ -14,9 +15,25 @@ const UserGallery = () => {
   return (
     <>
       <div className="row mx-auto">
-        {userData.map((item) => {
-          return <UserBox key={item.id} item={item} />;
-        })
+        {
+          loading || userData.length === 0 ?
+            <div className="d-flex justify-content-center">
+              <ThreeDots
+                height="80"
+                width="80"
+                radius="9"
+                color="#000000"
+                ariaLabel="three-dots-loading"
+                wrapperStyle={{}}
+                wrapperClassName=""
+                visible={true}
+              />
+            </div>
+
+            :
+            userData.map((item) => {
+              return <UserBox key={item.id} item={item} />;
+            })
         }
       </div>
     </>
